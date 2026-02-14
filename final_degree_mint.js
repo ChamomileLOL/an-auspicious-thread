9f9f591815a57037aa6fbe37b725692f3235a3be
Xavier Moorkattil <xavier.siby777@gmail.com>
2026-02-15 00:13:27 +0530

    SECURITY: Purged hardcoded credentials from history

--- final_degree_mint.js
+++ final_degree_mint.js
@@ -1,7 +1,19 @@
+require('dotenv').config();
+const { Web3 } = require('web3');
+const meritAbi = require('./abi/MeritToken.json');
+
+// THE SEPOLIA CONNECTION - Initializing 'web3' (lowercase)
+const web3 = new Web3(process.env.SEPOLIA_RPC_URL); 
+const contractAddress = "0xC2b503463CAe48279351044B928F60023B0b0196";
+const accountAddress = "0xEcb81DE5711b46eb3cc28afA2ad6e6fd31114988";
+const privateKey = process.env.PRIVATE_KEY; 
+
+const contract = new web3.eth.Contract(meritAbi, contractAddress);
+
 async function mintDegree() {
     console.log("--- INITIATING ON-CHAIN DEGREE MINTING ---");
     
-    // Get current gas price from the network
+    // Fetching the current gas price to prevent MissingGasError
     const gasPrice = await web3.eth.getGasPrice();
     
     const data = contract.methods.mintDegree(accountAddress, "BACHELOR OF TECHNOLOGY").encodeABI();
@@ -10,9 +22,9 @@ async function mintDegree() {
         from: accountAddress,
         to: contractAddress,
         gas: 500000,
-        gasPrice: gasPrice, // Added gasPrice to fix MissingGasError
+        gasPrice: gasPrice,
         data: data,
-        type: '0x0' // Specifies a legacy transaction type to simplify parameters
+        type: '0x0' 
     };
 
     console.log("Signing transaction...");
@@ -25,4 +37,6 @@ async function mintDegree() {
     console.log(`[TRANSACTION HASH]: ${receipt.transactionHash}`);
     console.log(`[VERIFY ON ETHERSCAN]: https://sepolia.etherscan.io/tx/${receipt.transactionHash}`);
 }
+
+// Ensure the function call is at the very bottom
 mintDegree().catch(console.error);
